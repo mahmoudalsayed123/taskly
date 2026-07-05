ee8f27f9df4129ce76990dd25ced1628d5771516
mahmoudalsayed123 <mahmoudalsayed05789@gmail.com>
2026-07-05 23:43:24 +0300

    fix design project page

--- app/(main)/_components/MainSection.tsx
+++ app/(main)/_components/MainSection.tsx
@@ -1,16 +1,10 @@
-import { getCurrentUser } from '@/lib/getCurrentUser';
 import Project from '../project/page';
-import { redirect } from 'next/navigation';
 
-const MainSection = async ({
+const MainSection = ({
   searchParams,
 }: {
   searchParams: Promise<{ page: string }>;
 }) => {
-  const user = await getCurrentUser();
-  if (user) {
-    return redirect('/project');
-  }
   return (
     <div className="relative pt-[64px] h-full w-full h-[calc(100vh - 64px)]">
       <Project searchParams={searchParams} />
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,12 +1,17 @@
+import { getCurrentUser } from '@/lib/getCurrentUser';
 import '../globals.css';
 import MainSection from '@/app/(main)/_components/MainSection';
+import { redirect } from 'next/navigation';
 
 const Page = async ({
   searchParams,
 }: {
   searchParams: Promise<{ page: string }>;
 }) => {
-
+  const user = await getCurrentUser();
+  if (user) {
+    return redirect('/project');
+  }
   return (
     <main className="col-span-6 md:col-span-6 md:ms-[200px] ">
       <MainSection searchParams={searchParams} />
