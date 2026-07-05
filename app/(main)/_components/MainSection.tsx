3e50d84c4cca177d88bc0f9770eeaeb79a96c3c0
mahmoudalsayed123 <mahmoudalsayed05789@gmail.com>
2026-07-05 23:21:42 +0300

    fix route /project on render website

--- app/(main)/_components/MainSection.tsx
+++ app/(main)/_components/MainSection.tsx
@@ -1,10 +1,16 @@
+import { getCurrentUser } from '@/lib/getCurrentUser';
 import Project from '../project/page';
+import { redirect } from 'next/navigation';
 
-const MainSection = ({
+const MainSection = async ({
   searchParams,
 }: {
   searchParams: Promise<{ page: string }>;
 }) => {
+  const user = await getCurrentUser();
+  if (user) {
+    return redirect('/project');
+  }
   return (
     <div className="relative pt-[64px] h-full w-full h-[calc(100vh - 64px)]">
       <Project searchParams={searchParams} />
